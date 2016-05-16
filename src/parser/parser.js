ca3a6ff9983445a3604c467bbae5b53d23529b39
Vadim Zhiltsov <vadim.zhiltsov@mail.ru>
2016-05-16 12:38:21 +0300

    add adhoc for windows-1251 encoding

--- src/parser/parser.js
+++ src/parser/parser.js
@@ -1,28 +1,29 @@
 const mp4Regex = /video\/mp4/;
 const Iconv = require('iconv').Iconv;
 const charsetRegex = /charset=(\S+)$/;
+const UTF8 = 'utf-8';
 
 export default class Parser {  
 
   constructor(window) {
-    try{
-        this.window = window;
-        this.$ = window.$;
-        this.encoding = this.getCharset();
-        
-        if (this.encoding !==  'utf-8') {
-          this._translator = new Iconv(this.encoding,'utf-8');
-        } else {
-          this._translator = {
-            convert(text) {
-              return text || "";
-            }
-          }
+    try {
+    this.window = window;
+    this.$ = window.$;
+    this.encoding = this.getCharset();
+    
+    if (this.encoding !==  UTF8 && this.encoding !== "windows-1251") {
+      this._translator = new Iconv(this.encoding, UTF8);
+    } else {
+      this._translator = {
+        convert(text) {
+          return text || "";
         }
+      }
+    }
 
-        this.data = this.parse();
+    this.data = this.parse();
     } catch(e) {
-        console.error(e, e.stack);
+      console.error(e, e.stack);
     }
   }
 
@@ -30,7 +31,7 @@ export default class Parser {
     const charsetStr = this.$('meta[http-equiv="content-type"]').attr('content');
     const charsetFindings = charsetRegex.exec(charsetStr);
 
-    return charsetFindings && charsetFindings.length ? charsetFindings[1] : 'utf-8';
+    return charsetFindings && charsetFindings.length ? charsetFindings[1] : UTF8;
   }
 
   parse(camera) {
@@ -47,7 +48,7 @@ export default class Parser {
 
   parseDesctiption(hash){
     let text = this.$('meta[name="description"]').attr('content');
-    let description = this._translator.convert(text).toString();
+    let description = this._translator.convert(text);
     
     if(description) {
         hash.description = description;
@@ -56,7 +57,7 @@ export default class Parser {
 
   parseTitle(hash){
     let text = this.$('title').text();
-    let title = this._translator.convert(text).toString();
+    let title = this._translator.convert(text);
     
     if(title) {
         hash.title = title;
